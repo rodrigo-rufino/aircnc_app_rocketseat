f1a0d904bb9c344c818531513bd248cec4b4fe07
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-01 19:18:16 -0300

    Creating Spots, Dashboard and User routes.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const multer = require('multer');
+
+const uploadConfig = require('./config/upload');
+
+const UserController = require("./controller/UserController");
+const SpotController = require("./controller/SpotController");
+const DashboardController = require("./controller/DashboardController");
 
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post('/users', (req, res) => {
-    return res.json(req.body);
-});
+routes.post('/sessions', UserController.store);
+
+routes.get('/spots', SpotController.index);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
 
 module.exports = routes;

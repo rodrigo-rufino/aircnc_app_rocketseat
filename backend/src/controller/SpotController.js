77864779f6d26c735bb77759095eabe139952776
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-01 19:18:47 -0300

    Creating DashBoard, Spot and User routes

---
+++ backend/src/controller/SpotController.js
@@ -0,0 +1,37 @@
+const Spot = require("../model/Spot");
+const User = require("../model/User");
+
+module.exports = {
+
+    async index(req, res){
+        const { tech } = req.query;
+
+        const spots = await Spot.find({ techs: tech});
+
+        return res.json(spots);
+    },
+
+
+    async store(req, res){
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        console.log(req.file);
+
+        const user = await User.findById(user_id);
+        if (!user) {
+            res.status(400).json({ error: "User doesn't exist."})
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price,
+        });
+
+        return res.json(spot);
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
+const dbConfig = require('./config/dbConfig');
 
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(dbConfig.dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 app.use(routes);
 
 

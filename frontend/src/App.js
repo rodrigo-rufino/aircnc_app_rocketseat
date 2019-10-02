93e4d24a6b68e8a2238043b15cd1f43faff07939
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-02 19:36:33 -0300

    [frontend] creating login submit + api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import api from "./services/api";
 import './App.css';
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault();
+
+    const response = api.post('/sessions', { email });
+
+    console.log(response);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -13,12 +23,18 @@ function App() {
           Ofereça <strong>spots</strong> para progamadores e encontre <strong>talentos</strong> para sua empresa.  
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail</label>
           <input
             type="email"
             id="email"
-            placeholder="E-mail"/>
+            placeholder="E-mail"
+            value={email}
+            // onChange={(event) => {
+            //   setEmail(event.target.value)
+            //   }}
+            onChange = { event => setEmail(event.target.value) }
+            />
           <button className="btn" type="submit">Entrar</button>
         </form>
 

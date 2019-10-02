991f58fdc97a98feb40994b4f7a5a0c0babd43ee
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-02 20:11:25 -0300

    [frontend] creating routes structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,9 @@ function App() {
 
     const response = await api.post('/sessions', { email });
 
-    console.log(response);
+    const { _id } = response.data;
+
+    localStorage.setItem('user', _id);
   }
 
   return (

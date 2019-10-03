4bf9165405491ebafa0ecc55a0ca68f1d55511ca
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-10-03 15:53:32 -0300

    [backend] files route

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 const routes = require('./routes');
 const dbConfig = require('./config/dbConfig');
 
@@ -19,6 +19,7 @@ mongoose.connect(dbConfig.dbUrl, {
 
 app.use(cors({ }));
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 

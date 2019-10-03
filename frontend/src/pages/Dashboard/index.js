5866ad941c3d3f0e73583fb777c9be299f084381
rodrigo-rufino <rodrigo.rufino@local.inatel.br>
2019-10-03 16:12:51 -0300

    [frontend] dashboard route

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './style.css';
 
 export default function Dashboard() {
-    return <div />
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    async function loadSpots() {
+      const user_id = localStorage.getItem('user');
+      const response = await api.get('/dashboard',{
+        headers: {
+          user_id
+        }
+      });
+      
+      setSpots(response.data);
+    }
+
+    loadSpots();
+  },
+  []); //variables which trigger this function
+
+  return (
+    <>
+    <ul className = "spot-list">
+      {spots.map((spot) => (
+        <li key={spot._id}>
+          <header style={{
+            background: `url(${spot.thumbnail_url})`
+          }}/>
+          <strong>{spot.company}</strong>
+          <span>{spot.price ? `R$${spot.price}/dia` : `Gratuito`}</span>
+        </li>
+      ))}
+    </ul>
+
+    <Link to="/new">
+      <button className='btn'>
+        Cadastrar novo Spot
+      </button>
+    </Link>
+    </>
+  )
 }

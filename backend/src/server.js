d36be977a2eeaffce4ecccbe9852453c7c17df25
rodrigo-rufino <rodrigo.srs.br@gmail.com>
2019-10-05 18:45:02 -0300

    [web_socket] setting up websocket communication

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,19 +13,24 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    console.log('Usuário conectado', socket.id);
-})
-
 mongoose.connect(dbConfig.dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).catch( (e) => {
-    console.log(e);
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch((e) => {
+  console.log(e);
 });
 
 
-app.use(cors({ }));
+
+//adiciona funcionalidade em todas as rotas
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+  
+  return next();
+});
+
+app.use(cors({}));
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
